db9c4ecf6002121e578b14d0cf21d3a1fab16c2b
phizercost <phizer.development@gmail.com>
2020-07-15 21:29:50 +0200

    Current User middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,21 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
+//Checking if the jwt is set
 router.get('/api/users/currentuser', (req, res) => {
-  res.send('currentuser API Being implemented');
+  if (!req.session?.jwt) {
+    return res.send({ currentUser: null });
+  }
+
+  //Checking if the jwt is valid
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    res.send({ currentUser: payload });
+  } catch (error) {
+    res.send({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };

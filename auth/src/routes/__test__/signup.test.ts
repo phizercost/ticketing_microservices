df33a2327d260bbc5da0d0cafae28b7a707ab96c
phizercost <phizer.development@gmail.com>
2020-07-20 17:44:14 +0200

    Tests implementation

---
+++ auth/src/routes/__test__/signup.test.ts
@@ -0,0 +1,70 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('returns a 201 on successful signup', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+});
+
+it('returns a 400 with an invalid email', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with an invalid password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'pa',
+    })
+    .expect(400);
+});
+
+it('returns a 400 with missing email and password', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({ email: 'phizer@phizer.com' })
+    .expect(400);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({ password: 'sdfasdfa' })
+    .expect(400);
+});
+
+it('disallows duplicate emails', async () => {
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(201);
+
+  await request(app)
+    .post('/api/users/signup')
+    .send({
+      email: 'test@test.com',
+      password: 'password',
+    })
+    .expect(400);
+});
+
+it('sets a cookie after succesfull signup', async () => {
+  const response = await request(app).post('/api/users/signup').send({
+    email: 'test@test.com',
+    password: 'password',
+  });
+  expect(response.get('Set-Cookie')).toBeDefined();
+});

a3da0ecb1f82ec3d308461f09ae9ed61b66be41f
phizercost <phizer.development@gmail.com>
2020-08-06 13:03:55 +0200

    Code sharing between services

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { currentUser } from '../middlewares/current-user';
+import { currentUser } from '@pdtickets/common';
 const router = express.Router();
 
 router.get('/api/users/currentuser', currentUser, (req, res) => {

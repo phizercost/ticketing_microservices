08b97453697286acda2a56f309531fcd590e0697
phizercost <phizer.development@gmail.com>
2020-08-21 17:31:49 +0200

    Setting the durable subscriptions for listeners

--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -14,10 +14,14 @@ stan.on('connect', () => {
     process.exit();
   });
 
-  const options = stan.subscriptionOptions().setManualAckMode(true);
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('accounting-service');
   const subscription = stan.subscribe(
     'ticket:created',
-    'orders-service-queue-group',
+    'queue-group-name',
     options
   );
   subscription.on('message', (msg: Message) => {

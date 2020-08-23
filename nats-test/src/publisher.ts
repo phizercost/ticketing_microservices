da8c185868fca8639e30440cac48753143c2aac2
phizercost <phizer.development@gmail.com>
2020-08-23 18:31:43 +0200

    Exporting the testes NATS functionalities to common module

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -6,22 +6,17 @@ const stan = nats.connect('ticketing', 'abc', {
   url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
   console.log('Publisher connected to NATS');
 
   const publisher = new TicketCreatedPublisher(stan);
-  publisher.publish({
-    id: '123',
-    title: 'concert',
-    price: 100,
-  });
-  // const data = JSON.stringify({
-  //   id: '123',
-  //   title: 'concert',
-  //   price: 20,
-  // });
-
-  // stan.publish('ticket:created', data, () => {
-  //   console.log('Event published');
-  // });
+  try {
+    await publisher.publish({
+      id: '123',
+      title: 'concert',
+      price: 100,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 });

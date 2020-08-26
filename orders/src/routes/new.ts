fba16a35a88576d1666e77642186cc009da79e93
phizercost <phizer.development@gmail.com>
2020-08-26 17:45:20 +0200

    Order model implementation with reference/population vs ticket

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,23 @@
 import express, { Request, Response } from 'express';
+import { requireAuth, validateRequest } from '@pdtickets/common';
+import { body } from 'express-validator';
+import mongoose from 'mongoose';
 
 const router = express.Router();
 
-router.post('/api/orders', async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  '/api/orders',
+  requireAuth,
+  [
+    body('ticketId')
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage('Ticket Id must be provided'),
+  ],
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };

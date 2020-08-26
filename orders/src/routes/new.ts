8c8e6025e7b5d8eac353834c98a5aab0bc022ea8
phizercost <phizer.development@gmail.com>
2020-08-26 19:11:07 +0200

    Testing the new order creation

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,18 @@
 import express, { Request, Response } from 'express';
-import { requireAuth, validateRequest } from '@pdtickets/common';
+import {
+  requireAuth,
+  validateRequest,
+  NotFoundError,
+  OrderStatus,
+  BadRequestError,
+} from '@pdtickets/common';
 import { body } from 'express-validator';
 import mongoose from 'mongoose';
+import { Ticket } from '../models/ticket';
+import { Order } from '../models/order';
 
 const router = express.Router();
+const EXPIRATION_WINDOW_SECONDS = 15 * 60;
 
 router.post(
   '/api/orders',
@@ -15,8 +24,39 @@ router.post(
       .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
       .withMessage('Ticket Id must be provided'),
   ],
+  validateRequest,
   async (req: Request, res: Response) => {
-    res.send({});
+    const { ticketId } = req.body;
+    //Find the ticket the user is trying to order in the database
+    const ticket = await Ticket.findById(ticketId);
+    if (!ticket) {
+      throw new NotFoundError();
+    }
+
+    // Make sure that this ticket is not already reserved
+    //Run quey to look at all orders. Find an order where the ticket is the
+    //ticket we just found and the orders status is not canncelled. If we find an order from that means the ticket is reserved
+    const isReserved = await ticket.isReserved();
+    if (isReserved) {
+      throw new BadRequestError('Ticket already reserved');
+    }
+
+    // Calculate the expiration time(date) for the order
+    const expiration = new Date();
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS);
+
+    //Build the order and save it in the database
+    const order = Order.build({
+      userId: req.currentUser!.id,
+      status: OrderStatus.Created,
+      expiresAt: expiration,
+      ticket,
+    });
+
+    await order.save();
+    // Publish an event showing that the order was created
+
+    res.status(201).send(order);
   }
 );
 

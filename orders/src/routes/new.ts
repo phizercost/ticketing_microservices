593e91c2144c28380b7d37cbba420c5b8518bee9
phizercost <phizer.development@gmail.com>
2020-08-30 21:44:19 +0200

    Error fixing for tests

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -59,6 +59,7 @@ router.post(
     // Publish an event showing that the order was created
     new OrderCreatedPublisher(natsWrapper.client).publish({
       id: order.id,
+      version: order.version,
       status: order.status,
       userId: order.userId,
       expiresAt: order.expiresAt.toISOString(),

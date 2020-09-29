8fe9fb80c60a5adea7123bc680edfc2106e4ad4b
phizercost <phizer.development@gmail.com>
2020-09-29 03:26:52 +0200

    Handling payments

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -4,6 +4,7 @@ import { json } from 'body-parser';
 
 import cookieSession from 'cookie-session';
 import { errorHandler, NotFoundError, currentUser } from '@pdtickets/common';
+import { createChargeRouter } from './routes/new';
 
 const app = express();
 app.set('trust proxy', true);
@@ -16,6 +17,7 @@ app.use(
 );
 
 app.use(currentUser);
+app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
   throw new NotFoundError();

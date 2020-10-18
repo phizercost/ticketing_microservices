fd719064b69128f6596f652f7d580ae15621e18e
phizercost <phizer.development@gmail.com>
2020-10-18 19:36:44 +0200

    Adding changes

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -6,6 +6,7 @@ import { OrderCreatedListener } from './events/listeners/order-created-listener'
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
+    console.log('Starting up ...')
     throw new Error('JWT_KEY must be defined');
   }
 

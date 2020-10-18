d2044c71f474ab3e12b73fc7932ae187b924e1ba
phizercost <phizer.development@gmail.com>
2020-10-18 22:35:05 +0200

    Changing the size fron h1 to h2

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+import Link from '/next/link';
 const LandingPage = ({ currentUser, tickets }) => {
   const ticketList = tickets.map((ticket) => {
     return (
@@ -15,7 +15,7 @@ const LandingPage = ({ currentUser, tickets }) => {
   });
   return (
     <div>
-      <h1>Tickets</h1>
+      <h2>Tickets</h2>
       <table className="table">
         <thead>
           <tr>
